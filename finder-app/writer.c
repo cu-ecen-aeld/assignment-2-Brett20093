b03810bf7aa9f950ce96c688cf9217d0fbe325c9
Brett Lange <brett.lange@colorado.edu>
2025-10-26 22:09:20 -0500

    Writer c code

---
+++ finder-app/writer.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <syslog.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+const uint8_t EXPECTED_NUM_ARGUMENTS = 3;
+
+int main(int argc, char *argv[])
+{
+    openlog(NULL, 0, LOG_USER);
+
+    if (argc != EXPECTED_NUM_ARGUMENTS)
+    {
+        printf("ERROR: Invalid Number of arguments. %d arguments are expected but %d were given.\n", EXPECTED_NUM_ARGUMENTS-1, argc-1);
+        syslog(LOG_ERR, "Invalid Number of arguments. %d arguments are expected but %d were given.", EXPECTED_NUM_ARGUMENTS-1, argc-1);
+        printf("Usage: %s <writefile> <searchstr>\n", argv[0]);
+        syslog(LOG_INFO, "Usage: %s <writefile> <searchstr>", argv[0]);
+        exit(1);
+    }
+
+    char* write_file = argv[1];
+    char* write_str = argv[2];
+
+    printf("Writing %s to %s\n", write_str, write_file);
+    syslog(LOG_DEBUG, "Writing %s to %s", write_str, write_file);
+
+    int fd = open(write_file, O_WRONLY | O_CREAT);
+    int write_return = write(fd, write_str, strlen(write_str));
+    if (write_return == -1)
+    {
+        perror("Failed to write to file.");
+        syslog(LOG_ERR, "Failed to write %s to file %s. errno: %d.", write_str, write_file, errno);
+        close(fd);
+        exit(1);
+    }
+
+    close(fd);
+
+    return 0;
+}
